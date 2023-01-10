c4710cc70b8abb50032b4b413718464f2513c100
Bryan <bryanpjpgi2b@hotmail.com>
2023-01-10 17:48:55 +0100

    project finished

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function hideElement(){
+    document.getElementById("triangulo").style.visibility = "hidden";
+}
+function hideSquare(){
+    document.getElementById("cuadrado").style.visibility = "hidden";
+}
+function hideCircle(){
+    document.getElementById("circulo").style.visibility = "hidden";
+}
+
+function reset(){
+    document.getElementById("triangulo").style.visibility = "visible";
+    document.getElementById("cuadrado").style.visibility = "visible";
+    document.getElementById("circulo").style.visibility = "visible";
+}
